531c6baf3a8deb5555886e5f2b7ca7578f5820ec
Rue-chelle <misshie21@gmail.com>
2025-03-19 09:52:55 +0200

    added real time data using moment

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,25 @@
+function updateTime() {
+  // harare
+  let harareElement = document.querySelector("#harare");
+  let harareDateElement = harareElement.querySelector(".date");
+  let harareTimeElement = harareElement.querySelector(".time");
+  let harareTime = moment().tz("Africa/Harare");
 
+  harareDateElement.innerHTML = harareTime.format("MMMM Do YYYY");
+  harareTimeElement.innerHTML = harareTime.format(
+    "h:mm:ss [<small>]A[<small/>] "
+  );
+
+  // Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format(
+    "h:mm:ss [<small>]A[<small/>] "
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
